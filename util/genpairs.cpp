e24fe771c11f5b3cf35dd8d5a619056e12d3d479
Ramy Shahin <r.shahin@computer.org>
2015-12-05 02:46:10 -0500

    Make sure we do not generate duplicate keys

--- util/genpairs.cpp
+++ util/genpairs.cpp
@@ -15,21 +15,23 @@ int main(int argc, char* argv[])
   }
 
   srand(time(NULL));
-
+ 
   size_t count = atoi(argv[1]);
   ofstream out(argv[2]);
 
-  vector<int> keys(count);
+  vector<int> keys;
+  keys.reserve(count);
   out << count << endl;
 
   size_t i = 0;
 
   while (i < count) {
-    int key = rand();
-    if (find(keys.begin(), keys.end(), key) != keys.end()) {
+    int key = rand() * rand();
+    if (key < 1 || find(keys.begin(), keys.end(), key) != keys.end()) {
       continue;
     }
 
+	keys.push_back(key);
     int val = rand();
 
     out << key << "\t" << val << endl;
